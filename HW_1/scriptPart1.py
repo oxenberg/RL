c53d3098d82ca83ddcfe5d6642522ad1c104a89e
Or Oxenberg <oxenberg14@gmail.com>
2020-11-11 20:51:14 +0200

    create infra

--- HW_1/scriptPart1.py
+++ HW_1/scriptPart1.py
@@ -5,3 +5,34 @@ Created on Wed Nov 11 20:40:20 2020
 @author: oxenb
 """
 
+
+class FrozenAgent():
+    def __init__():
+        '''
+        parans:
+            alpha (float) - 
+            lambda (float) -
+
+        -------
+
+        '''
+    def train():
+        '''
+        maxEpochs (float) -
+        
+        Returns
+        -------
+        None.
+
+        '''
+    def createGraphs():
+        '''
+        1.Plot of the reward per episode.
+        2.Plot of the average number of steps to the goal over last 100 episodes
+
+        Returns
+        -------
+        None.
+
+        '''
+    
