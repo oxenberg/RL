19c3e35c7dbe832897b68b2fa110783a9d9f6cbb
Or Oxenberg <oxenberg14@gmail.com>
2020-11-12 14:56:40 +0200

    delete duplicate train func

--- HW_1/scriptPart1.py
+++ HW_1/scriptPart1.py
@@ -16,15 +16,7 @@ class FrozenAgent:
         '''
         self.env = gym.make('FrozenLake-v0')
 
-    def train(self, maxEpochs=10, alpha=0.01, lambd=0.97, maxSteps=100):
-        self.num_actions = self.env.action_space.n
-        self.Qtable = np.zeros(
-            (self.env.observation_space.n, self.num_actions))
-        # ToDo: check is_slippery variable in make
-        # ToDo: verify on which board size we play
 
-        self.actions = [i for i in range(self.env.action_space.n)]
-        self.states = [i for i in range(self.env.observation_space.n)]
 
     def _sampleActionFromQtable(self, state: int, epsilon):
         best_action = np.argmax(self.Qtable[state, :])
@@ -32,7 +24,7 @@ class FrozenAgent:
                                  for i in range(self.num_actions)]
         return np.random.choice(self.actions, p=sampling_distribution)
 
-    def train(self, maxEpochs=10, alpha=0.01, lambd=0.97, epsilon=0.1):
+    def train(self, maxEpochs=10, alpha=0.01,epsilon = 0.98, lambd=0.01, maxSteps=100):
         '''
         params:
 
@@ -46,7 +38,17 @@ class FrozenAgent:
         None.
 
         '''
-        self.maxEpochs = 800
+        self.num_actions = self.env.action_space.n
+        self.Qtable = np.zeros(
+            (self.env.observation_space.n, self.num_actions))
+        # ToDo: check is_slippery variable in make
+        # ToDo: verify on which board size we play
+        # ToDo: verify hyperparameter values
+
+        self.actions = [i for i in range(self.env.action_space.n)]
+        self.states = [i for i in range(self.env.observation_space.n)]
+
+        self.maxEpochs = maxEpochs
         self.rewards = []
         self.stepsPerEpoch = []
         self.QtablesSample = {}
@@ -54,27 +56,23 @@ class FrozenAgent:
         sampleSteps = [200, 500]
         currentState = self.env.reset()
         overallSteps = 0
-        for epoch in range(maxEpochs):
-            step = 0
-            while(True):
-
-                randomAction = self.env.action_space.sample()
-        # ToDo: verify hyperparameter values
-        self.maxEpochs = 10
         self.rewards = []
         self.stepsPerEpoch = []
 
         currentState = self.env.reset()
         for _ in range(maxEpochs):
             step = 0
+            overallReward = 0
             while (True):
                 randomAction = self._sampleActionFromQtable(
                     currentState, epsilon)
                 newState, reward, done, info = self.env.step(randomAction)
+                overallReward += reward
 
+                
                 if done or step == maxSteps:
                     self.stepsPerEpoch.append(step)
-                    self.rewards.append(reward)
+                    self.rewards.append(overallReward)
 
                     self.Qtable[currentState, randomAction] = alpha * \
                         (reward - self.Qtable[currentState, randomAction])
@@ -91,7 +89,7 @@ class FrozenAgent:
 
                 currentState = newState
                 step += 1
-                self.env.render()
+                # self.env.render()
 
             if overallSteps in sampleSteps:
                 self.QtablesSample[overallSteps] = self.Qtable
@@ -119,7 +117,7 @@ class FrozenAgent:
         try:
             averageWindow = np.mean(
                 np.array(self.stepsPerEpoch).reshape(-1, averageOver), axis=1)
-            EpisodesSteps = np.arange(0, len(self.maxEpochs), 100)
+            EpisodesSteps = np.arange(0, self.maxEpochs, 100)
             plt.plot(EpisodesSteps, averageWindow)
             plt.xlabel("episode")
             plt.ylabel("[steps]")
@@ -128,15 +126,24 @@ class FrozenAgent:
         except:
             print("cant create graph 2 due of wrong divide number for the window")
 
-        fig, ax = plt.subplots(3)
-        overallSteps = self.QtablesSample.keys()[-1]
+        fig, ax = plt.subplots(figsize=(12, 12))
+        overallSteps = list(self.QtablesSample.keys())[-1]
         finaleQtable = self.QtablesSample[overallSteps]
+        print(finaleQtable)
         im = ax.imshow(finaleQtable)
+        states = list(map(str,np.arange(finaleQtable.shape[0])))
+        actions = list(map(str,np.arange(finaleQtable.shape[1])))
 
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(len(actions)))
+        ax.set_yticks(np.arange(len(states)))
+        # ... and label them with the respective list entries
+        ax.set_xticklabels(actions)
+        ax.set_yticklabels(states)
         # Loop over data dimensions and create text annotations.
-        for i in range(len(finaleQtable)):
-            for j in range(len(finaleQtable)):
-                text = ax.text(j, i, finaleQtable[i, j],
+        for i in range(len(states)):
+            for j in range(len(actions)):
+                text = ax.text(j, i, round(np.log(finaleQtable[i, j]),2),
                                ha="center", va="center", color="w")
 
         ax.set_title("final Q table")
@@ -155,5 +162,5 @@ class FrozenAgent:
 
 if __name__ == '__main__':
     agent = FrozenAgent()
-    agent.train(5000)
-    print()
+    agent.train(maxEpochs = 5000)
+    agent.createGraphs()
