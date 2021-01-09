bcc8f9fadc997d9fa8657c1a9c86afeca47514d6
efrat <efrat@intezer.com>
2021-01-09 12:46:36 +0200

    hw3: fix mountaincar actor critic

--- HW3/train_acrobot.py
+++ HW3/train_acrobot.py
@@ -1,8 +1,12 @@
 from HW3.actor_critic import Agent
 from HW3.actor_critic import OpenGymEnvs
 
-if __name__ == '__main__':
+
+def train_acrobot():
     agent = Agent(OpenGymEnvs.ACROBOT)
     best_parameters = {'discount_factor': 0.95, 'learning_rate': 0.001, 'learning_rate_value': 0.0001,
                        'num_hidden_layers': 3, 'num_neurons_value': 64, 'num_neurons_policy': 12}
-    ret = agent.run(**best_parameters, for_transfer=True)
+    ret = agent.run(**best_parameters)
+
+if __name__ == '__main__':
+    train_acrobot()
